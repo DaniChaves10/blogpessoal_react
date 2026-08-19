ebbf02beb8c7b799027d28e0b08c9b097403e664
Daniel Araujo Chaves <danielaraujo.da789@gmail.com>
2026-08-19 14:51:26 -0300

    ✨ New Feature: Finalizando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
-import Home from "./pages/home/Home"
 import Cadastro from "./pages/cadastro/Cadastro"
+import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 
 
@@ -15,9 +15,9 @@ function App() {
       <Navbar />
       <div className="min-h-[80vh]">
       <Routes >
-        <Route path='/' element={<Home />} />
-        <Route path='/cadastro' element={<Cadastro />} />
-        <Route path='/login' element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
       </div>
       <Footer />
