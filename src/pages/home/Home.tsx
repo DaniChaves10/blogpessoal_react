225705523f659b5f8dd29ceb2b85a9857ee7ee4a
Daniel Araujo Chaves <danielaraujo.da789@gmail.com>
2026-08-25 14:25:01 -0300

    ✨ New Feature: Criando a Postagem, editar e deletar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,10 @@
-
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem"
 
 function Home() {
 
   return (
-
+    <>
     /* Container Principal */
     <div className="bg-indigo-900 flex justify-center">
 
@@ -13,16 +14,16 @@ function Home() {
             {/* Conteúdo de texto */}
             <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className="text-2xl md:text-5xl font-bold">
-                Seja Bem Vinde
+                Seja Bem Vindo!
             </h2>
 
             <p className="text-xl">
-                Expresse aqui seus pensamentos
+                Expresse aqui seus pensamentos e opniões
             </p>
 
             <div className="flex justify-around gap-4">
-                <div className="rounded-lg border-2 border-solid border-white py-2 px-4">
-                    Nova Postagem
+                <div className="rounded text-white border-white border-solid border-2 py-2 px-4">
+                     <ModalPostagem />
                 </div>
             </div>
             </div>
@@ -35,12 +36,10 @@ function Home() {
                 className="w-2/3"
                 />
             </div>
-
-
         </div>
-
-
     </div>
+     <ListaPostagens />
+     </>
   )
 }
 
